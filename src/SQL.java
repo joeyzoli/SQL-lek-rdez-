5501f7e3dffe8c26bd3aff1b60f6dd1a7b3e312d
joeyzoli <joeyayeti@gmail.com>
2023-04-28 11:33:30 +0200

    xml adatok
    új táblában keres, most xml adatokat kérdez le.

--- src/SQL.java
+++ src/SQL.java
@@ -51,7 +51,8 @@ public class SQL
 			SQL_lekerdezo.szazalek = 70;
 			//SQL_lekerdezo.progressBar.setValue(10);
 			//Preparing a CallableStatement to call a procedure
-			// CallableStatement cstmt = con.prepareCall("{call videoton.veas_avmheti_teszt(?)}");									
+			// CallableStatement cstmt = con.prepareCall("{call videoton.veas_avmheti_teszt(?)}");
+			/*
 			String sql = "select 	videoton.fkov.azon, videoton.fkov.hely,videoton.fkovsor.nev, videoton.fkov.ido, videoton.fkov.panel, cast(videoton.fkov.alsor as char(5)) as Teszterszam,"
 					+ "if(videoton.fkov.ok in ('-1', '1'), \"Rendben\", \"Hiba\") as eredmeny, "
 					+ "videoton.fkov.hibakod, videoton.fkov.kod2, videoton.fkov.torolt, "
@@ -60,7 +61,8 @@ public class SQL
 					+ "from	videoton.fkov \n"
 					+ "inner join videoton.fkovsor on videoton.fkovsor.azon = videoton.fkov.hely \n"
 					+ " where panel in (" + osszefuzott +")";
-			
+			*/
+			String sql = "select * from videoton.fkovadat where fkov in ("+ osszefuzott +")";
 			String sql2 = "select 	videoton.fkov.azon, videoton.fkov.hely,videoton.fkovsor.nev, videoton.fkov.ido, videoton.fkov.panel, cast(videoton.fkov.alsor as char(5)) as Teszterszam,"
 					+ "if(videoton.fkov.ok in ('-1', '1'), \"Rendben\", \"Hiba\") as eredmeny, "
 					+ "videoton.fkov.hibakod, videoton.fkov.kod2, videoton.fkov.torolt, "
